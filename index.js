f5687fb50f0cb04d19d0245ccbaf255625145dd2
Rich <richbport@yahoo.com>
2025-09-11 13:43:10 -0400

    handleDebouncingScroll

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function handleThrottleScroll() {
     throttleCount++
     document.getElementById('ThrottleCount').textContent = throttleCount
 }
+
+function handleDebouncingScroll() {
+    document.getElementById("debounceState").textContent = "Scrolling...";
+    setTimeout(() => {
+        document.getElementById("debounceState").textContent = "Not Scrolling..."
+    }, 200)
+}
