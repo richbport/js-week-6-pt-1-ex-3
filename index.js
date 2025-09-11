5da6b0fb0d990af8206b3d1eedd7d4a7baa52d92
Rich <richbport@yahoo.com>
2025-09-11 13:46:35 -0400

    throttle

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+function throttle(func, limit) {
+    let inThrottle;
+    return function () {
+        const args = arguments;
+        const context = this;
+        if (!inThrottle) {
+            func.apply(context, args);
+            inThrottle = true;
+            setTimeout(() => (inThrottle = false), limit);
+        }
+    };
+}
+
+
 
 
 
